ac3c5f729ae6bf90d1882b962a6e724c0e792c27
Randy Abernethy <randy.abernethy@rx-m.com>
2021-06-20 08:24:53 -0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,9 +1,8 @@
 # To install go: $ curl https://raw.githubusercontent.com/RX-M/classfiles/master/go.sh | sh
 
-cd ~
 wget https://golang.org/dl/go1.16.linux-amd64.tar.gz							
 tar zxf go1.16.linux-amd64.tar.gz							
-sudo mv ~/go/ /usr/local/							
+sudo mv ./go/ /usr/local/							
 echo "export PATH=/usr/local/go/bin:$PATH" >> ~/.bashrc							
 echo "[[ -r ~/.bashrc ]] && . ~/.bashrc" >> ~/.bash_profile							
 . ~/.bash_profile							
