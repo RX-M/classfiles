f4158c6add3b73df8a5b53dc98e2500035a311e0
Ronald Petty <ronald.petty@rx-m.com>
2021-07-02 17:58:05 -0400

    Update go.sh
    I am wrong, upgrading to bash, not sure how '[[' is being interpreted, yet it is.

--- go.sh
+++ go.sh
@@ -1,9 +1,10 @@
-# To install go: $ curl https://raw.githubusercontent.com/RX-M/classfiles/master/go.sh | sh
+#!/bin/bash
 
+# To install go: $ curl https://raw.githubusercontent.com/RX-M/classfiles/master/go.sh | sh
 wget https://golang.org/dl/go1.16.linux-amd64.tar.gz							
 tar zxf go1.16.linux-amd64.tar.gz							
 sudo mv ./go/ /usr/local/							
 echo "export PATH=/usr/local/go/bin:$PATH" >> ~/.bashrc							
-echo '[[ -r ~/.bashrc ]] && . ~/.bashrc' >> ~/.bash_profile							
+echo "[[ -r ~/.bashrc ]] && . ~/.bashrc" >> ~/.bash_profile							
 . ~/.bash_profile							
 go version
