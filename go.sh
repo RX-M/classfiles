a0432044bbfd99b027889d79a68f04836b6317d5
Ronald Petty <ronald.petty@rx-m.com>
2021-07-02 17:50:42 -0400

    Update go.sh
    Fixing shell issues, mainly "[[" to '[[' because in 'sh' that fails but in 'bash' it works. Was going to change magic to /bin/bash but figured leave it.

--- go.sh
+++ go.sh
@@ -4,6 +4,6 @@ wget https://golang.org/dl/go1.16.linux-amd64.tar.gz
 tar zxf go1.16.linux-amd64.tar.gz							
 sudo mv ./go/ /usr/local/							
 echo "export PATH=/usr/local/go/bin:$PATH" >> ~/.bashrc							
-echo "[[ -r ~/.bashrc ]] && . ~/.bashrc" >> ~/.bash_profile							
+echo '[[ -r ~/.bashrc ]] && . ~/.bashrc' >> ~/.bash_profile							
 . ~/.bash_profile							
 go version
