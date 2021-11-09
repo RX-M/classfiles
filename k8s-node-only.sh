72b9ad0666cc6cdf59c70657877aea8dc4052675
Ronald Petty <ronald.petty@rx-m.com>
2021-11-09 14:37:54 -0800

    Update k8s-node-only.sh
    Added versioning:
    
    curl -s https://raw.githubusercontent.com/RX-M/classfiles/master/k8s-node-only.sh | K8S_VERSION=v1.22.3 bash -

--- k8s-node-only.sh
+++ k8s-node-only.sh
@@ -4,7 +4,6 @@ echo "Basic worker node setup"
 echo "This script works in conjunction with https://github.com/RX-M/classfiles/blob/master/k8s.sh"
 
 sudo apt-get update
-sudo apt-get install -y kubeadm
 wget -qO- https://get.docker.com/ | sh
 
 cat <<EOF | sudo tee /etc/docker/daemon.json
@@ -19,10 +18,13 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 EOF
 
 sudo systemctl restart docker
+
+# kubeadm, kubelet, and company
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubeadm
+if [ -z ${K8S_VERSION+x} ]; then K8S_VERSION="1.22.3-00" ; else K8S_VERSION="$K8S_VERSION"; fi
+sudo apt install -y kubeadm=$K8S_VERSION kubectl=$K8S_VERSION kubelet=$K8S_VERSION kubernetes-cni
 sudo swapoff -a
 
-echo "Run on control plane to retrieve join command: kubeadm token create --print-join-command"
+printf "To retrieve join command, run on control plane:\nkubeadm token create --print-join-command\n"
