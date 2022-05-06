9622fe0219ce97e92f237972c442db813870840e
Christian Alday Lacsina <christian.lacsina@rx-m.com>
2022-05-06 14:09:21 -0700

    switch k8s.sh and k8s-node-only.sh to cri-dockerd

--- k8s-node-only.sh
+++ k8s-node-only.sh
@@ -19,6 +19,26 @@ EOF
 
 sudo systemctl restart docker
 
+# Install cri-dockerd
+VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest|grep tag_name | cut -d '"' -f 4)
+wget https://github.com/Mirantis/cri-dockerd/releases/download/${VER}/cri-dockerd-${VER}-linux-amd64.tar.gz
+tar xvf cri-dockerd-${VER}-linux-amd64.tar.gz
+sudo mv cri-dockerd /usr/local/bin/
+sudo wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/50c048cb54e52cd9058f044671e309e9fbda82e4/packaging/systemd/cri-docker.service
+sudo wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/50c048cb54e52cd9058f044671e309e9fbda82e4/packaging/systemd/cri-docker.socket
+sudo mv cri-docker.socket cri-docker.service /etc/systemd/system/
+sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+sudo mkdir -p /etc/systemd/system/cri-docker.service.d/
+cat <<EOF | sudo tee /etc/systemd/system/cri-docker.service.d/cni.conf
+[Service]
+ExecStart=
+ExecStart=/usr/local/bin/cri-dockerd --container-runtime-endpoint fd:// --network-plugin=cni --cni-bin-dir=/opt/cni/bin --cni-cache-dir=/var/lib/cni/cache --cni-conf-dir=/etc/cni/net.d
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable cri-docker.service
+sudo systemctl enable --now cri-docker.socket
+
+
 # kubeadm, kubelet, and company
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
@@ -30,3 +50,4 @@ sudo apt-get install -y kubeadm
 sudo swapoff -a
 
 printf "To retrieve join command, run on control plane:\nkubeadm token create --print-join-command\n"
+printf "Make sure to append to the end: \n--cri-socket=unix:///var/run/cri-dockerd.sock\n"
--- k8s.sh
+++ k8s.sh
@@ -26,7 +26,10 @@
 # limitations under the License.
 
 set -e
+
+# Install Docker
 wget -qO- https://get.docker.com/ | sh
+
 cat <<EOF | sudo tee /etc/docker/daemon.json
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
@@ -38,15 +41,37 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 }
 EOF
 sudo systemctl restart docker
+
+# Install cri-dockerd
+VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest|grep tag_name | cut -d '"' -f 4)
+wget https://github.com/Mirantis/cri-dockerd/releases/download/${VER}/cri-dockerd-${VER}-linux-amd64.tar.gz
+tar xvf cri-dockerd-${VER}-linux-amd64.tar.gz
+sudo mv cri-dockerd /usr/local/bin/
+sudo wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/50c048cb54e52cd9058f044671e309e9fbda82e4/packaging/systemd/cri-docker.service
+sudo wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/50c048cb54e52cd9058f044671e309e9fbda82e4/packaging/systemd/cri-docker.socket
+sudo mv cri-docker.socket cri-docker.service /etc/systemd/system/
+sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+sudo mkdir -p /etc/systemd/system/cri-docker.service.d/
+cat <<EOF | sudo tee /etc/systemd/system/cri-docker.service.d/cni.conf
+[Service]
+ExecStart=
+ExecStart=/usr/local/bin/cri-dockerd --container-runtime-endpoint fd:// --network-plugin=cni --cni-bin-dir=/opt/cni/bin --cni-cache-dir=/var/lib/cni/cache --cni-conf-dir=/etc/cni/net.d
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable cri-docker.service
+sudo systemctl enable --now cri-docker.socket
+
+# Initialize a control plane node
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubeadm
 sudo swapoff -a
 if [ -z ${K8S_VERSION+x} ]; then K8S_VERSION="--kubernetes-version=stable-1" ; else K8S_VERSION="--kubernetes-version=$K8S_VERSION"; fi
-sudo kubeadm init $K8S_VERSION
+sudo kubeadm init --cri-socket=unix:///run/cri-dockerd.sock  $K8S_VERSION
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 kubectl taint node --all node-role.kubernetes.io/master-
+kubectl taint node --all node-role.kubernetes.io/control-plane-
