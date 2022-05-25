160c7a0dcf2e3bc63ffb88dc41f2eeb1ce0b7aa1
Christian Alday Lacsina <christian.lacsina@rx-m.com>
2022-05-25 13:33:27 -0700

    k8s-ckad fix cri-dockerd link

--- k8s-ckad.sh
+++ k8s-ckad.sh
@@ -43,9 +43,9 @@ EOF
 sudo systemctl restart docker
 
 # Install cri-dockerd
-VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest|grep tag_name | cut -d '"' -f 4)
-wget https://github.com/Mirantis/cri-dockerd/releases/download/${VER}/cri-dockerd-${VER}-linux-amd64.tar.gz
-tar xvf cri-dockerd-${VER}-linux-amd64.tar.gz
+VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest|grep tag_name | cut -d '"' -f 4 | cut -b 2-)
+wget https://github.com/Mirantis/cri-dockerd/releases/download/${VER}/cri-dockerd-${VER}.amd64.tgz
+tar xvf cri-dockerd-${VER}.amd64.tgz
 sudo mv cri-dockerd /usr/local/bin/
 sudo wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/50c048cb54e52cd9058f044671e309e9fbda82e4/packaging/systemd/cri-docker.service
 sudo wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/50c048cb54e52cd9058f044671e309e9fbda82e4/packaging/systemd/cri-docker.socket
