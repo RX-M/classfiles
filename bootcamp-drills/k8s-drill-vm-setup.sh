e3f48977744aeedf25a127a94d28e546a7a760c2
Christian Alday Lacsina <christian.lacsina@rx-m.com>
2022-08-21 18:27:17 -0700

    add script to set up extra node for cka drills

---
+++ bootcamp-drills/k8s-drill-vm-setup.sh
@@ -0,0 +1,68 @@
+# Script to install a single node K8s cluster on an RX-M Lab VM
+#
+# To use:  $ curl https://raw.githubusercontent.com/RX-M/classfiles/master/k8s.sh | sh
+#
+# N.B. The script turns off swap for the K8s control plane install but does
+#      not disable swap permenantly. Please comment out any swap volumes in
+#      the /etc/fstab before rebooting the VM.
+#
+#      This script will fail to run if the apt db is locked (wait 10 mins
+#      and retry or reboot and retry).
+#
+#      Kubernetes single node clusters require a 4GB ram VM to run properly.
+#
+# Copyright (c) 2021 RX-M LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+# Install Docker
+wget -qO- https://get.docker.com/ | sh
+
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+sudo systemctl restart docker
+
+# Install cri-dockerd
+VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest|grep tag_name | cut -d '"' -f 4 | cut -b 2-)
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz
+tar xvf cri-dockerd-${VER}.amd64.tgz
+sudo mv cri-dockerd /usr/local/bin/
+sudo wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/50c048cb54e52cd9058f044671e309e9fbda82e4/packaging/systemd/cri-docker.service
+sudo wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/50c048cb54e52cd9058f044671e309e9fbda82e4/packaging/systemd/cri-docker.socket
+sudo mv cri-docker.socket cri-docker.service /etc/systemd/system/
+sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+sudo mkdir -p /etc/systemd/system/cri-docker.service.d/
+cat <<EOF | sudo tee /etc/systemd/system/cri-docker.service.d/cni.conf
+[Service]
+ExecStart=
+ExecStart=/usr/local/bin/cri-dockerd/cri-dockerd --container-runtime-endpoint fd:// --network-plugin=cni --cni-bin-dir=/opt/cni/bin --cni-cache-dir=/var/lib/cni/cache --cni-conf-dir=/etc/cni/net.d
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable cri-docker.service
+sudo systemctl enable --now cri-docker.socket
+
+# Initialize a control plane node
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubeadm=1.23.0-00 kubectl=1.23.0-00 kubelet=1.23.0-00
