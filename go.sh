bf66e4e99e899721a46d5c48898d4bcb129af106
Ronald Petty <ronald.petty@rx-m.com>
2023-03-12 11:54:17 -0700

    Update go.sh
    update to 1.20.2

--- go.sh
+++ go.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # To install go: $ curl https://raw.githubusercontent.com/RX-M/classfiles/master/go.sh | sh
-wget https://go.dev/dl/go1.18.2.linux-amd64.tar.gz
-tar zxf go1.18.2.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
+tar zxf go1.20.2.linux-amd64.tar.gz
 sudo mv ./go/ /usr/local/							
 echo "export PATH=/usr/local/go/bin:$PATH" >> ~/.bashrc							
 echo "[[ -r ~/.bashrc ]] && . ~/.bashrc" >> ~/.bash_profile							
