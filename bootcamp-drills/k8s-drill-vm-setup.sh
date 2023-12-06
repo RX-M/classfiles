226e4aef61063771c53d948fcf263d8f5baf064b
Christian Alday Lacsina <christian.lacsina@rx-m.com>
2023-12-06 16:35:30 -0600

    increment k8s-drill-vm-setup to 1.27

--- bootcamp-drills/k8s-drill-vm-setup.sh
+++ bootcamp-drills/k8s-drill-vm-setup.sh
@@ -52,4 +52,4 @@ sudo systemctl restart containerd
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubeadm=1.26.0-00 kubectl=1.26.0-00 kubelet=1.26.0-00
+sudo apt-get install -y kubeadm=1.27.0-00 kubectl=1.27.0-00 kubelet=1.27.0-00
