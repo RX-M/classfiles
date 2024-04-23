23f724fc0391ff2f557a347e344e6afa60df1203
Christian Alday Lacsina <christian.lacsina@rx-m.com>
2024-04-22 22:27:41 -0700

    bump ckad k8s script to 1.29

--- k8s-ckad.sh
+++ k8s-ckad.sh
@@ -1,17 +1,17 @@
+#!/bin/bash
+#
 # Script to install a single node K8s cluster on an RX-M Lab VM
 #
-# To use:  $ curl https://raw.githubusercontent.com/RX-M/classfiles/master/k8s.sh | sh
+# Usage:  $ curl https://raw.githubusercontent.com/RX-M/classfiles/master/k8s.sh | sh
 #
-# N.B. The script turns off swap for the K8s control plane install but does
-#      not disable swap permenantly. Please comment out any swap volumes in
-#      the /etc/fstab before rebooting the VM.
+# N.B. The script turns off swap for the K8s control plane install but does not disable swap permenantly.
+#      Please comment out any swap volumes in the /etc/fstab before rebooting the VM.
 #
-#      This script will fail to run if the apt db is locked (wait 10 mins
-#      and retry or reboot and retry).
+#      This script will fail to run if the apt db is locked (wait 10 mins and retry or reboot and retry).
 #
 #      Kubernetes single node clusters require a 4GB ram VM to run properly.
 #
-# Copyright (c) 2021-2023 RX-M LLC
+# Copyright (c) 2021-2024 RX-M LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,12 +25,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 set -e
 
-export K8S_VERSION=1.27.4
+# Defaults
+DOCKER_VER="25.0.2"
+K8S_VERSION="v1.29.4"
+K8S_REPO="https://pkgs.k8s.io/core:/stable:/v1.29/deb"
+WEAVE_VER="v2.8.1"
+WEAVE_DS="weave-daemonset-k8s-1.11.yaml"
+WEAVE_REPO="https://github.com/weaveworks/weave/releases/download"
 
 # Install Docker
-wget -qO- https://get.docker.com/ | sh
+curl -fsSL https://get.docker.com -o /tmp/install-docker.sh && sh /tmp/install-docker.sh --version $DOCKER_VER
+
 sudo mkdir -p /etc/docker
 cat <<EOF | sudo tee /etc/docker/daemon.json
 {
@@ -51,22 +59,28 @@ sudo sed -i -e 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/c
 sudo systemctl restart containerd
 
 # Initialize a control plane node
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubeadm=$K8S_VERSION-00 kubectl=$K8S_VERSION-00 kubelet=$K8S_VERSION-00
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL "${K8S_REPO}/Release.key" | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] ${K8S_REPO}/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
 sudo swapoff -a
-if [ -z ${K8S_VERSION+x} ]; then K8S_VERSION="--kubernetes-version=stable-1" ; else K8S_VERSION="--kubernetes-version=$K8S_VERSION"; fi
-sudo kubeadm init --cri-socket=unix:///var/run/containerd/containerd.sock  $K8S_VERSION
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s-1.11.yaml
-kubectl patch node $(hostname) -p '{"spec":{"taints":[]}}'
+if [ -z "${K8S_VERSION+x}" ]; then K8S_VERSION="stable-1"; fi
+
+sudo kubeadm init --cri-socket=unix:///var/run/containerd/containerd.sock --kubernetes-version="${K8S_VERSION}"
+mkdir -p "${HOME}/.kube"
+sudo cp -i /etc/kubernetes/admin.conf "${HOME}/.kube/config"
+sudo chown "$(id -u):$(id -g)" "${HOME}/.kube/config"
+kubectl apply -f "${WEAVE_REPO}/${WEAVE_VER}/${WEAVE_DS}"
+kubectl patch node "$(hostname)" -p '{"spec":{"taints":[]}}'
 
 # Install the latest crictl (cni-tools package is not always the latest)
-CRICTL_VERSION=$(curl -s https://api.github.com/repos/kubernetes-sigs/cri-tools/releases/latest|grep tag_name | cut -d '"' -f 4 | cut -b 2-)
-wget https://github.com/kubernetes-sigs/cri-tools/releases/download/v$CRICTL_VERSION/crictl-v$CRICTL_VERSION-linux-amd64.tar.gz
-sudo tar zxvf crictl-v$CRICTL_VERSION-linux-amd64.tar.gz -C /usr/local/bin
-rm -f crictl-v$CRICTL_VERSION-linux-amd64.tar.gz
+crictl_ver=$(curl -s https://api.github.com/repos/kubernetes-sigs/cri-tools/releases/latest | grep tag_name | cut -d '"' -f 4 | cut -b 2-)
+wget "https://github.com/kubernetes-sigs/cri-tools/releases/download/v${crictl_ver}/crictl-v${crictl_ver}-linux-amd64.tar.gz"
+sudo tar zxvf "crictl-v${crictl_ver}-linux-amd64.tar.gz" -C /usr/local/bin
+rm -f "crictl-v${crictl_ver}-linux-amd64.tar.gz"
 echo "runtime-endpoint: unix:///run/containerd/containerd.sock" | sudo tee /etc/crictl.yaml
