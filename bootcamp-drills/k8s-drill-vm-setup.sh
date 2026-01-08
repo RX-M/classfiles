05c464656f139a580a3164a6543ed7ebeb5c64fd
Christopher Hanson <christopher.hanson@rx-m.com>
2026-01-08 14:28:45 -0800

    Bump K8s and etcd versions

--- bootcamp-drills/k8s-drill-vm-setup.sh
+++ bootcamp-drills/k8s-drill-vm-setup.sh
@@ -11,7 +11,7 @@
 #
 #      Kubernetes single node clusters require a 4GB ram VM to run properly.
 #
-# Copyright (c) 2025 RX-M LLC
+# Copyright (c) 2026 RX-M LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -46,12 +46,12 @@ sudo systemctl restart docker
 sudo cp /etc/containerd/config.toml /etc/containerd/config.bak
 sudo containerd config default | sudo tee /etc/containerd/config.toml
 sudo sed -i -e 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
-sudo sed -i -e 's/pause:3.8/pause:3.10/' /etc/containerd/config.toml
+sudo sed -i -e 's/pause:3.8/pause:3.10.1/' /etc/containerd/config.toml
 sudo systemctl restart containerd
 
 # Prepare the binaries to init a control plane node
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.34/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubeadm kubectl kubelet
 
@@ -65,7 +65,7 @@ sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
 rm cilium-linux-${CLI_ARCH}.tar.gz.sha256sum cilium-linux-${CLI_ARCH}.tar.gz
 
 # Add etcdctl and etcdutl to the machine
-wget https://github.com/etcd-io/etcd/releases/download/v3.5.21/etcd-v3.5.21-linux-amd64.tar.gz
+wget https://github.com/etcd-io/etcd/releases/download/v3.6.4/etcd-v3.6.4-linux-amd64.tar.gz
 tar xzf etcd-*-linux-amd64.tar.gz --strip-components=1
 sudo cp etcdctl /usr/bin/
 sudo cp etcdutl /usr/bin/
